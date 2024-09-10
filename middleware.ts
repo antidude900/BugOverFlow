bd18d808b19e7c5a3682f2a5f0868091604ca311
antidude900 <splashofwater900@gmail.com>
2024-09-10 16:56:21 +0545

    a

--- middleware.ts
+++ middleware.ts
@@ -7,13 +7,16 @@ const isPublicRoute = createRouteMatcher([
 	"/api/webhook(.*)",
 ]);
 
-
 export default clerkMiddleware((auth, req) => {
 	if (isPublicRoute(req)) return; // if it's a public route, do nothing
 	auth().protect();
-  
 });
 
 export const config = {
-	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*),", "/api/webhook(.*)",],
+	matcher: [
+		"/((?!.*\\..*|_next).*)",
+		"/",
+		"/(api|trpc)(.*),",
+		"/api/webhook(.*)"
+	],
 };
